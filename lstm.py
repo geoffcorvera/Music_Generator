51585caa46f4df8b879793c7e0020e30ba193e33
Geoffrey Corvera <geoffcorvera@gmail.com>
2020-12-07 17:17:27 -0800

    initialise LSTM

--- lstm.py
+++ lstm.py
@@ -19,4 +19,57 @@ idx_to_char = {i: w for i,w in enumerate(chars)}
 
 # Activation Layer
     # each category is a binary switch (thresholding at 0.5) to allow for multiple notes to be on at a time
-# 
+
+
+class LSTM:
+
+    def __init__(self, char_to_idx, idx_to_char, vocab_size, n_h=100, seq_len=25, epochs=10, lr=0.01, beta1=0.9, beta2=0.999):
+        self.char_to_idx = char_to_idx      # char/note to indices mapping
+        self.idx_to_char = idx_to_char      # indices to char/note mapping
+        self.vocab_size = vocab_size        # no. of classes/unique values in training data
+        self.n_h = n_h                      # no. units in hidden layer
+        self.seq_len = seq_len              # no. of time steps
+        self.epochs = epochs                # no. of training iterations
+        self.lr = lr                        # learning rate
+        self.beta1 = beta1                  # 1st momentum param
+        self.beta2 = beta2                  # 2nd momentum param
+    
+        # Initialise weights & biases
+        self.param = {}
+        std = (1.0/np.sqrt(self.vocab_size + self.n_h))     # Xavier initialisation
+
+        # Forget gate
+        self.params = {}
+        self.params["Wf"] = np.random.randn(self.n_h, self.n_h + self.vocab_size) * std
+        self.params["bf"] = np.ones((self.n_h, 1))
+
+        # Input gate
+        self.params["Wi"] = np.random.randn(self.n_h, self.n_h + self.vocab_size) * std
+        self.params["bi"] = np.ones((self.n_h, 1))
+
+        # Cell gate
+        self.params["Wc"] = np.random.randn(self.n_h, self.n_h + self.vocab_size) * std
+        self.params["bc"] = np.zeros((self.n_h, 1))
+
+        # Output gate
+        self.params["Wo"] = np.random.randn(self.n_h, self.n_h + self.vocab_size) * std
+        self.params["bo"] = np.zeros((self.n_h, 1))
+
+        # Output
+        # XXX: re-examine output activations
+        self.params["Wv"] = np.random.randn(self.vocab_size, self.n_h) * (1.0/np.sqrt(self.vocab_size))
+        self.params["bv"] = np.zeros((self.vocab_size, 1))
+
+        # Initialise gradients and Adam parameters
+        self.grads = {}
+        self.adam_params = {}
+
+        for key in self.params:
+            self.grads["d"+key] = np.zeros_like(self.params[key])
+            self.adam_params["m"+key] = np.zeros_like(self.params[key])
+            self.adam_params["v"+key] = np.zeros_like(self.params[key])
+
+        self.smooth_loss = -np.log(1.0 / self.vocab_size) * self.seq_len
+
+
+lstm = LSTM(char_to_idx, idx_to_char, vocab_size)
