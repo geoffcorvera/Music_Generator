d0424cd83b04c8f1ca3f020b6aa903f558b00fbf
Geoffrey Corvera <geoffcorvera@gmail.com>
2020-12-08 20:24:07 -0800

    output trained weights & biases after training

--- generator.py
+++ generator.py
@@ -1,6 +1,7 @@
 import os
 from music21 import converter, instrument, note, chord, midi
 import numpy as np
+import matplotlib.pyplot as plt
 from lstm import LSTM
 
 
@@ -41,7 +42,7 @@ def open_midi(midi_path):
     return midi.translate.midiFileToStream(mf)
 
 
-def getDebussy():
+def processTestMidi():
     notes = []
     prefix = 'data/test'
     for f in os.listdir(prefix):
@@ -53,7 +54,7 @@ def getDebussy():
 
     
 # Extract notes from Debussy folder
-notes = getDebussy()
+notes = processTestMidi()
 pitchnames = sorted(set(item for item in notes))
 n_pitches = len(pitchnames)
 
@@ -61,25 +62,58 @@ n_pitches = len(pitchnames)
 note_to_int = dict((nt, num) for num, nt in enumerate(pitchnames))
 int_to_note = dict((num, nt) for num, nt in enumerate(pitchnames))
 
+model = LSTM(note_to_int, int_to_note, n_pitches, epochs=100, lr=0.01)
+error, params = model.train(notes)
 
-# TODO: experiment with different sequence lengths
-seq_len = 100
+# Output trained model parameters
+# TODO: test
+for key in params:
+    filename = f"{str(key)}.csv"
+    print(f'output {str(key)} to "output/{filename}"')
+    np.savetxt(filename, params[key], delimiter=",")
 
-network_input = []
-network_output = []
+plt.plot([i for i in range(len(error))], error)
+plt.xlabel('#training iterations')
+plt.ylabel('training loss')
+plt.show()
 
-# Create input sequences
-for i in range(len(notes) - seq_len):
-    seq_in = notes[i:i+seq_len]
-    seq_out = notes[i+seq_len]
-    network_input.append([note_to_int[nt] for nt in seq_in])
-    network_output.append(note_to_int[seq_out])
 
-n_sequences = len(network_input)
-# Reshape input to LSTM compatible format
-network_input = np.reshape(network_input, (n_sequences, seq_len, 1))
-# Normalize input
-network_input = network_input /float(n_pitches)
-network_output = oneHotEncoding(network_output, n_pitches)
+def test():
+    # TODO: experiment with different sequence lengths
+    seq_len = 100
 
-lstm = LSTM(note_to_int, int_to_note, n_pitches)
+    network_input = []
+    network_output = []
+
+    # Create input sequences
+    for i in range(len(notes) - seq_len):
+        seq_in = notes[i:i+seq_len]
+        seq_out = notes[i+seq_len]
+        network_input.append([note_to_int[nt] for nt in seq_in])
+        network_output.append(note_to_int[seq_out])
+
+    n_sequences = len(network_input)
+    # Reshape input to LSTM compatible format
+    network_input = np.reshape(network_input, (n_sequences, seq_len, 1))
+    # Normalize input
+    network_input = network_input /float(n_pitches)
+    network_output = oneHotEncoding(network_output, n_pitches)
+
+
+"""
+data = open('data/austen-pride-and-prejudice.txt', 'r', encoding='utf-8').read().lower()
+
+chars = set(data)
+vocab_size = len(chars)
+print(f'data has {len(data)} characters, {vocab_size} unique')
+
+char_to_idx = {w: i for i,w in enumerate(chars)}
+idx_to_char = {i: w for i,w in enumerate(chars)}
+
+model = LSTM(char_to_idx, idx_to_char, vocab_size, epochs=5, lr=0.01)
+J, params = model.train(data)
+
+plt.plot([i for i in range(len(J))], J)
+plt.xlabel("#training iterations")
+plt.ylabel("training loss")
+"""
