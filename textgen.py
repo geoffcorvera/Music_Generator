dd09fbcd905e4d3d3a2eeedd31e6c68bfa252057
Geoffrey Corvera <geoffcorvera@gmail.com>
2020-12-09 11:36:38 -0800

    Move pride and prejudice test to separate file

---
+++ textgen.py
@@ -0,0 +1,18 @@
+from lstm import LSTM
+import matplotlib.pyplot as plt
+
+data = open('data/austen-pride-and-prejudice.txt', 'r', encoding='utf-8').read().lower()
+
+chars = set(data)
+vocab_size = len(chars)
+print(f'data has {len(data)} characters, {vocab_size} unique')
+
+char_to_idx = {w: i for i,w in enumerate(chars)}
+idx_to_char = {i: w for i,w in enumerate(chars)}
+
+model = LSTM(char_to_idx, idx_to_char, vocab_size, epochs=5, lr=0.01)
+J, params = model.train(data)
+
+plt.plot([i for i in range(len(J))], J)
+plt.xlabel("#training iterations")
+plt.ylabel("training loss")
